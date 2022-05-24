0ab75ddb995d501170d37d7f4516bbd4a0a45762
Praneeth <malpraneeth@deloitte.com>
2022-05-25 00:46:30 +0530

    Admin Page and Reading Excel

---
+++ MainAssignmentPytest/AdminPage.py
@@ -0,0 +1,79 @@
+from ReadingExcel import Excel
+from MainAssignment import TimeCaluclation
+
+
+class AdminClass(Excel):
+    def welcomeAdmin(self):
+        print("******Welcome Admin*******\n1. Add New Movie Information\n2. Edit Movie Info\n3. Delete Movies\n4.Logout")
+
+    def addMovie(self, info_new_movie):
+        super().writeExcel(info_new_movie)
+
+    def editMovie(self):
+        super().editExcel()
+
+    def deleteMovie(self):
+        super().delExcel()
+
+
+def adminAction():
+    while True:
+        # created obj of admin class
+        admin = AdminClass()
+        admin.welcomeAdmin()
+        opt = int(input("enter option: "))
+        if opt == 1:
+            print("Add movie:")
+            print("******Welcome Admin******* ")
+            info_new_movie = []
+            title = input("enter movie name: ")
+            info_new_movie.append(title)
+
+            genre = input("enter genre: ")
+            info_new_movie.append(genre)
+
+            length = input("enter the duration in hrs:min format")
+            info_new_movie.append(length)
+
+            cast = input("enter Cast: ")
+            info_new_movie.append(cast)
+
+            director = input("enter director name: ")
+            info_new_movie.append(director)
+
+            admin_rating = input("enter admin rating: ")
+            info_new_movie.append(admin_rating)
+
+            language = input("enter language")
+            info_new_movie.append(language)
+
+            interval_time = input("enter interval-time should be in number :")
+            info_new_movie.append(interval_time)
+
+            shows = input("enter no of shows")
+            info_new_movie.append(shows)
+
+            first_show = input("enter first show timing in hr:min:sec format: ")
+            info_new_movie.append(first_show)
+
+            gap = input("enter gap b/w the shows: ")
+            info_new_movie.append(gap)
+
+            timings = TimeCaluclation.calTime(shows, first_show, gap, interval_time, length)
+            info_new_movie.append(timings)
+
+            capacity = input("enter the total tickets: ")
+            info_new_movie.append(capacity)
+
+
+            admin.addMovie(info_new_movie)
+        elif opt == 2:
+            print("Edit movie:")
+            print("******Welcome Admin******* ")
+            admin.editMovie()
+        elif opt == 3:
+            print("Delete movie:")
+            print("******Welcome Admin******* ")
+            admin.deleteMovie()
+        else:
+            break
